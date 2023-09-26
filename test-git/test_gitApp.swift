e0f23c83982bfce889e02aa66a80e7bea169bce6
husnajakeer <husnajakeer@gmail.com>
2023-09-26 18:39:09 -0400

    Initial Commit

---
+++ test-git/test_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_gitApp.swift
+//  test-git
+//
+//  Created by Husna Jakeer Hussain on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct test_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
